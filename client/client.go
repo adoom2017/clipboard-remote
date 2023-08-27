2cc014ae335a5398d701d30e9612fe99fec05b13
adoom <shendongchun08@gmail.com>
2023-08-27 12:04:45 +0800

    add config files

--- client/client.go
+++ client/client.go
@@ -19,127 +19,134 @@ import (
 )
 
 var (
-    addr = flag.String("addr", "localhost:443", "remote address")
-    id   = flag.String("id", "1", "user id")
+	configPath = flag.String("config", "client.yaml", "client config file path")
 )
 
 func init() {
-    // Set the log output to stdout
-    log.SetReportCaller(true)
-    log.SetFormatter(&util.Formatter{
-        HideKeys:    true,
-        CallerFirst: true,
-        NoColors:    true,
-    })
+	// Set the log output to stdout
+	log.SetReportCaller(true)
+	log.SetFormatter(&util.Formatter{
+		HideKeys:    true,
+		CallerFirst: true,
+		NoColors:    true,
+	})
 
-    // Set the log output to the specified file
-    log.SetOutput(os.Stdout)
+	// Set the log output to the specified file
+	log.SetOutput(os.Stdout)
 
-    // Set the log level
-    log.SetLevel(log.InfoLevel)
+	// Set the log level
+	log.SetLevel(log.InfoLevel)
 }
 
 func main() {
-    flag.Parse()
+	flag.Parse()
 
-    // add interrupt sigal
-    interrupt := make(chan os.Signal, 1)
-    signal.Notify(interrupt, os.Interrupt)
+	clientConfig, err := util.ClientConfigRead(*configPath)
+	if err != nil {
+		log.Errorf("Failed to load client config file(%s), err: %v.", *configPath, err)
+		return
+	}
 
-    tlsConfig := tls.Config{
-        InsecureSkipVerify: true,
-    }
-    dial := websocket.Dialer{TLSClientConfig: &tlsConfig}
+	// add interrupt sigal
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt)
 
-    u := url.URL{Scheme: "wss", Host: *addr, Path: "/"}
-    log.Infof("connecting to %s", u.String())
+	tlsConfig := tls.Config{
+		InsecureSkipVerify: clientConfig.InsecureSkipVerify,
+	}
+	dial := websocket.Dialer{TLSClientConfig: &tlsConfig}
 
-    reqHeader := make(http.Header)
-    reqHeader.Add("ID", *id)
+	u := url.URL{Scheme: "wss", Host: clientConfig.Host, Path: clientConfig.WebsocketPath}
+	log.Infof("Succeed connecting to %s, uuid: %s.", u.String(), clientConfig.Uuid)
 
-    c, _, err := dial.Dial(u.String(), reqHeader)
-    if err != nil {
-        log.Fatalln(err)
-    }
-    defer c.Close()
+	reqHeader := make(http.Header)
+	reqHeader.Add("ID", clientConfig.Uuid)
+	reqHeader.Add("User-Name", clientConfig.Auth.User)
+	reqHeader.Add("Token", clientConfig.Auth.Password)
 
-    done := make(chan struct{})
+	c, _, err := dial.Dial(u.String(), reqHeader)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer c.Close()
 
-    // ping handler for keepalive
-    c.SetPingHandler(func(message string) error {
-        log.Infoln("Receive ping message from server:", c.RemoteAddr().String())
-        err := c.WriteControl(websocket.PongMessage, []byte(message), time.Now().Add(10*time.Second))
-        if err == websocket.ErrCloseSent {
-            return nil
-        } else if _, ok := err.(net.Error); ok {
-            return nil
-        }
-        return err
-    })
+	done := make(chan struct{})
 
-    // receive clipboard content from server
-    // then add the content to local clipboard
-    go func() {
-        defer close(done)
-        for {
-            _, message, err := c.ReadMessage()
-            if err != nil {
-                log.Errorln("Read error:", err)
-                return
-            }
+	// ping handler for keepalive
+	c.SetPingHandler(func(message string) error {
+		log.Infoln("Receive ping message from server:", c.RemoteAddr().String())
+		err := c.WriteControl(websocket.PongMessage, []byte(message), time.Now().Add(10*time.Second))
+		if err == websocket.ErrCloseSent {
+			return nil
+		} else if _, ok := err.(net.Error); ok {
+			return nil
+		}
+		return err
+	})
 
-            clipInfo, err := util.DecodeToStruct(message)
-            if err != nil {
-                log.Errorln("Unsupported message:", err)
-                continue
-            }
+	// receive clipboard content from server
+	// then add the content to local clipboard
+	go func() {
+		defer close(done)
+		for {
+			_, message, err := c.ReadMessage()
+			if err != nil {
+				log.Errorln("Read error:", err)
+				return
+			}
 
-            switch clipInfo.Type {
-            case util.CLIP_TEXT:
-                log.Infof("Recv Text Message: %s", string(clipInfo.Buff))
-            case util.CLIP_PATH:
-                log.Infoln("Recv Path Message.")
-            default:
-                log.Infoln("Unsupported format:", clipInfo.Type)
-            }
-        }
-    }()
+			clipInfo, err := util.DecodeToStruct(message)
+			if err != nil {
+				log.Errorln("Unsupported message:", err)
+				continue
+			}
 
-    // watch context, used for watch break
-    ctx, cancel := context.WithCancel(context.Background())
-    defer cancel()
+			switch clipInfo.Type {
+			case util.CLIP_TEXT:
+				log.Infof("Recv Text Message: %s", string(clipInfo.Buff))
+			case util.CLIP_PATH:
+				log.Infoln("Recv Path Message.")
+			default:
+				log.Infoln("Unsupported format:", clipInfo.Type)
+			}
+		}
+	}()
 
-    dataCh := clipboard.Watch(ctx)
+	// watch context, used for watch break
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 
-    for {
-        select {
-        case <-done:
-            log.Infoln("Bye bye!")
-            return
-        case data, ok := <-dataCh:
-            if !ok {
-                log.Errorln("Clipboard data channel has been closed.")
-                return
-            }
+	dataCh := clipboard.Watch(ctx)
 
-            c.SetWriteDeadline(time.Now().Add(10 * time.Second))
-            err := c.WriteMessage(websocket.BinaryMessage, data)
-            if err != nil {
-                log.Errorln("Failed to write message:", err)
-                return
-            }
-            log.Infoln("Send clipboard message succeed.")
+	for {
+		select {
+		case <-done:
+			log.Infoln("Bye bye!")
+			return
+		case data, ok := <-dataCh:
+			if !ok {
+				log.Errorln("Clipboard data channel has been closed.")
+				return
+			}
 
-        case <-interrupt:
-            log.Infoln("Interrupt manually by user.")
+			c.SetWriteDeadline(time.Now().Add(10 * time.Second))
+			err := c.WriteMessage(websocket.BinaryMessage, data)
+			if err != nil {
+				log.Errorln("Failed to write message:", err)
+				return
+			}
+			log.Infoln("Send clipboard message succeed.")
 
-            // Cleanly close the connection by sending a close message and then
-            // waiting (with timeout) for the server to close the connection.
-            err := c.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
-            if err != nil {
-                log.Errorln("Failed to send close message:", err)
-                return
-            }
-        }
-    }
+		case <-interrupt:
+			log.Infoln("Interrupt manually by user.")
+
+			// Cleanly close the connection by sending a close message and then
+			// waiting (with timeout) for the server to close the connection.
+			err := c.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
+			if err != nil {
+				log.Errorln("Failed to send close message:", err)
+				return
+			}
+		}
+	}
 }
--- common/config.go
+++ common/config.go
@@ -1,67 +1,116 @@
 package common
 
 import (
-    "os"
+	"os"
 
-    log "github.com/sirupsen/logrus"
+	log "github.com/sirupsen/logrus"
 
-    "github.com/google/uuid"
-    "gopkg.in/yaml.v3"
+	"github.com/google/uuid"
+	"gopkg.in/yaml.v3"
 )
 
-// Config clipboard client config
-type Config struct {
-    Host               string     `yaml:"server"`
-    Port               int        `yaml:"port"`
-    WebsocketPath      string     `yaml:"websocket-path"`
-    Log                LogConfig  `yaml:"log"`
-    Auth               AuthConfig `yaml:"auth"`
-    Uuid               string     `yaml:"uuid"`
-    InsecureSkipVerify bool       `yaml:"skip-cert-verify"`
-    Mode               string     `yaml:"mode"`
+// ClientConfig clipboard client config
+type ClientConfig struct {
+	Host               string     `yaml:"server"`
+	Port               int        `yaml:"port"`
+	WebsocketPath      string     `yaml:"websocket-path"`
+	Log                LogConfig  `yaml:"log"`
+	Auth               AuthConfig `yaml:"auth"`
+	Uuid               string     `yaml:"uuid"`
+	InsecureSkipVerify bool       `yaml:"skip-cert-verify"`
+	Mode               string     `yaml:"mode"`
+}
+
+// ServerConfig clipboard server config
+type ServerConfig struct {
+	Auths         []AuthConfig `yaml:"auths"`
+	MaxMsgSize    int          `yaml:"max-msg-size"`
+	ListenPort    int          `yaml:"listen"`
+	WebsocketPath string       `yaml:"websocket-path"`
+	Certificate   CertConfig   `yaml:"certificate"`
+	Log           LogConfig    `yaml:"log"`
+}
+
+// CertConfig config the certificate files
+type CertConfig struct {
+	CertFile string `yaml:"cert-file"`
+	KeyFile  string `yaml:"key-file"`
 }
 
 // LogConfig log config, default is stdOut, level is info
 type LogConfig struct {
-    LogPath  string `yaml:"path"`
-    LogLevel string `yaml:"log-level"`
+	LogPath  string `yaml:"path"`
+	LogLevel string `yaml:"log-level"`
 }
 
 // AuthConfig auth config
 type AuthConfig struct {
-    User     string `yaml:"user"`
-    Password string `yaml:"password"`
+	User     string `yaml:"user"`
+	Password string `yaml:"password"`
 }
 
-// ConfigRead read the client config, and set default value
-func ConfigRead(configFile string) (*Config, error) {
-    // Read the config file
-    data, err := os.ReadFile(configFile)
-    if err != nil {
-        log.Errorln("Falied to read config file:", configFile)
-        return nil, err
-    }
+// ClientConfigRead read the client config, and set default value
+func ClientConfigRead(configFile string) (*ClientConfig, error) {
+	// Read the config file
+	data, err := os.ReadFile(configFile)
+	if err != nil {
+		log.Errorln("Falied to read config file:", configFile)
+		return nil, err
+	}
 
-    // Parse config file
-    var config Config
-    err = yaml.Unmarshal(data, &config)
-    if err != nil {
-        log.Errorln("Failed to parse config file.")
-        return nil, err
-    }
+	// Parse config file
+	var config ClientConfig
+	err = yaml.Unmarshal(data, &config)
+	if err != nil {
+		log.Errorln("Failed to parse config file.")
+		return nil, err
+	}
 
-    // Set the default value
-    if config.WebsocketPath == "" {
-        config.WebsocketPath = "/"
-    }
+	// Set the default value
+	if config.WebsocketPath == "" {
+		config.WebsocketPath = "/"
+	}
 
-    if config.Log.LogLevel == "" {
-        config.Log.LogLevel = "info"
-    }
+	if config.Log.LogLevel == "" {
+		config.Log.LogLevel = "info"
+	}
 
-    if config.Uuid == "" {
-        config.Uuid = uuid.New().String()
-    }
+	if config.Uuid == "" {
+		config.Uuid = uuid.New().String()
+	}
 
-    return &config, nil
+	return &config, nil
+}
+
+// ServerConfigRead read the server config, and set default value
+func ServerConfigRead(configFile string) (*ServerConfig, error) {
+	// Read the config file
+	data, err := os.ReadFile(configFile)
+	if err != nil {
+		log.Errorln("Falied to read config file:", configFile)
+		return nil, err
+	}
+
+	// Parse config file
+	var config ServerConfig
+	err = yaml.Unmarshal(data, &config)
+	if err != nil {
+		log.Errorln("Failed to parse config file.")
+		return nil, err
+	}
+
+	// Set the default value
+	if config.WebsocketPath == "" {
+		config.WebsocketPath = "/"
+	}
+
+	if config.Log.LogLevel == "" {
+		config.Log.LogLevel = "info"
+	}
+
+	if config.MaxMsgSize == 0 {
+		config.MaxMsgSize = 10 * 1024 * 1024
+	}
+
+	return &config, nil
 }
