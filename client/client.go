1bac8f02fdd338cc37316d48c7ce9148d3ed30cb
adoom <shendongchun08@gmail.com>
2023-09-04 18:53:40 +0800

    modify client code

--- client/client.go
+++ client/client.go
@@ -1,162 +1,62 @@
 package main
 
 import (
-    "context"
-    "crypto/tls"
-    "flag"
-    "net"
-    "net/http"
-    "net/url"
-    "os"
-    "os/signal"
-    "time"
+	"context"
+	"flag"
+	"os"
+	"os/signal"
 
-    "clipboard-remote/clipboard"
-    util "clipboard-remote/common"
+	"clipboard-remote/clipboard"
+	util "clipboard-remote/common"
 
-    "github.com/gorilla/websocket"
-    log "github.com/sirupsen/logrus"
-    "golang.org/x/crypto/bcrypt"
+	log "github.com/sirupsen/logrus"
 )
 
 var (
-    configPath = flag.String("config", "client.yaml", "client config file path")
+	configPath = flag.String("config", "client.yaml", "client config file path")
 )
 
 func init() {
-    // Set the log output to stdout
-    log.SetReportCaller(true)
-    log.SetFormatter(&util.Formatter{
-        HideKeys:    true,
-        CallerFirst: true,
-        NoColors:    true,
-    })
+	// Set the log output to stdout
+	log.SetReportCaller(true)
+	log.SetFormatter(&util.Formatter{
+		HideKeys:    true,
+		CallerFirst: true,
+		NoColors:    true,
+	})
 
-    // Set the log output to the specified file
-    log.SetOutput(os.Stdout)
+	// Set the log output to the specified file
+	log.SetOutput(os.Stdout)
 
-    // Set the log level
-    log.SetLevel(log.InfoLevel)
+	// Set the log level
+	log.SetLevel(log.InfoLevel)
 }
 
 func main() {
-    flag.Parse()
+	flag.Parse()
 
-    clientConfig, err := util.ClientConfigRead(*configPath)
-    if err != nil {
-        log.Errorf("Failed to load client config file(%s), err: %v.", *configPath, err)
-        return
-    }
+	clientConfig, err := util.ClientConfigRead(*configPath)
+	if err != nil {
+		log.Errorf("Failed to load client config file(%s), err: %v.", *configPath, err)
+		return
+	}
 
-    // add interrupt sigal
-    interrupt := make(chan os.Signal, 1)
-    signal.Notify(interrupt, os.Interrupt)
+	// add interrupt sigal
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt)
 
-    tlsConfig := tls.Config{
-        InsecureSkipVerify: clientConfig.InsecureSkipVerify,
-    }
-    dial := websocket.Dialer{TLSClientConfig: &tlsConfig}
+	// watch context, used for watch break
+	ctx, cancel := context.WithCancel(context.Background())
+	//go func() {
+	//}()
 
-    u := url.URL{Scheme: "wss", Host: clientConfig.Host, Path: clientConfig.WebsocketPath}
-    // log.Infof("Succeed connecting to %s, uuid: %s.", u.String(), clientConfig.Uuid)
+	// handle io local to server
+	client := NewClient(clientConfig)
+	client.handleIO(ctx, clipboard.Watch(ctx))
 
-    reqHeader := make(http.Header)
-    //reqHeader.Add("ID", clientConfig.Uuid)
-    reqHeader.Add("User-Name", clientConfig.Auth.User)
+	<-interrupt
+	cancel()
+	log.Infoln("Interrupt manually by user.")
 
-    hashBytes, err := bcrypt.GenerateFromPassword([]byte(clientConfig.Auth.Password), bcrypt.DefaultCost)
-    if err != nil {
-        log.Errorln("Failed to hash password.")
-        return
-    }
-
-    reqHeader.Add("Token", string(hashBytes))
-
-    c, _, err := dial.Dial(u.String(), reqHeader)
-    if err != nil {
-        log.Fatalln(err)
-    }
-    defer c.Close()
-
-    done := make(chan struct{})
-
-    // ping handler for keepalive
-    c.SetPingHandler(func(message string) error {
-        // log.Infoln("Receive ping message from server:", c.RemoteAddr().String())
-        err := c.WriteControl(websocket.PongMessage, []byte(message), time.Now().Add(10*time.Second))
-        if err == websocket.ErrCloseSent {
-            return nil
-        } else if _, ok := err.(net.Error); ok {
-            return nil
-        }
-        return err
-    })
-
-    // receive clipboard content from server
-    // then add the content to local clipboard
-    go func() {
-        defer close(done)
-        for {
-            _, message, err := c.ReadMessage()
-            if err != nil {
-                log.Errorln("Read error:", err)
-                return
-            }
-
-            clipInfo, err := util.DecodeToStruct(message)
-            if err != nil {
-                log.Errorln("Unsupported message:", err)
-                continue
-            }
-
-            switch clipInfo.Type {
-            case util.CLIP_TEXT:
-                log.Infof("Recv Text Message: %s", string(clipInfo.Buff))
-                clipboard.Write(message)
-                log.Infof("Set Text Message: %s to clipboard.", string(clipInfo.Buff))
-            case util.CLIP_PATH:
-                log.Infoln("Recv Path Message.")
-            default:
-                log.Errorln("Unsupported format:", clipInfo.Type)
-            }
-        }
-    }()
-
-    // watch context, used for watch break
-    ctx, cancel := context.WithCancel(context.Background())
-    defer cancel()
-
-    dataCh := clipboard.Watch(ctx)
-
-    for {
-        select {
-        case <-done:
-            log.Infoln("Bye bye!")
-            return
-        case data, ok := <-dataCh:
-            if !ok {
-                log.Errorln("Clipboard data channel has been closed.")
-                return
-            }
-
-            c.SetWriteDeadline(time.Now().Add(10 * time.Second))
-            err := c.WriteMessage(websocket.BinaryMessage, data)
-            if err != nil {
-                log.Errorln("Failed to write message:", err)
-                return
-            }
-            log.Infoln("Send clipboard message succeed.")
-
-        case <-interrupt:
-            log.Infoln("Interrupt manually by user.")
-
-            // Cleanly close the connection by sending a close message and then
-            // waiting (with timeout) for the server to close the connection.
-            err := c.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
-            if err != nil {
-                log.Errorln("Failed to send close message:", err)
-                return
-            }
-        }
-    }
+	client.close()
 }
--- client/ws.go
+++ client/ws.go
@@ -1,225 +1,259 @@
 package main
 
 import (
-    "context"
-    "crypto/tls"
-    "encoding/base64"
-    "fmt"
-    "net/http"
-    "net/url"
-    "os"
-    "sync"
-    "time"
+	"context"
+	"crypto/tls"
+	"encoding/base64"
+	"fmt"
+	"net/http"
+	"net/url"
+	"os"
+	"sync"
+	"time"
 
-    "github.com/google/uuid"
-    "github.com/gorilla/websocket"
-    log "github.com/sirupsen/logrus"
-    "golang.org/x/crypto/bcrypt"
+	"github.com/google/uuid"
+	"github.com/gorilla/websocket"
+	log "github.com/sirupsen/logrus"
+	"golang.org/x/crypto/bcrypt"
 
-    "clipboard-remote/clipboard"
-    util "clipboard-remote/common"
+	"clipboard-remote/clipboard"
+	util "clipboard-remote/common"
 )
 
 type Client struct {
-    sync.Mutex
+	sync.Mutex
 
-    client *util.ClientConfig
-    ID     string
-    conn   *websocket.Conn
+	client *util.ClientConfig
+	ID     string
+	conn   *websocket.Conn
 
-    // {string: chan *types.WebsocketMessage}
-    readChs sync.Map
-    writeCh chan *util.WebsocketMessage
+	// {string: chan *types.WebsocketMessage}
+	readChs sync.Map
+	writeCh chan *util.WebsocketMessage
 }
 
 // NewClient creates a new ws client
 func NewClient(c *util.ClientConfig) *Client {
-    id, err := os.Hostname()
-    if err != nil {
-        id = uuid.NewString()
-        if err != nil {
-            panic(fmt.Errorf("failed to initialize daemon: %v", err))
-        }
-    }
-    return &Client{
-        ID:      id,
-        writeCh: make(chan *util.WebsocketMessage, 10),
-        client:  c,
-    }
+	id, err := os.Hostname()
+	if err != nil {
+		id = uuid.NewString()
+		if err != nil {
+			panic(fmt.Errorf("failed to initialize daemon: %v", err))
+		}
+	}
+	return &Client{
+		ID:      id,
+		writeCh: make(chan *util.WebsocketMessage, 10),
+		client:  c,
+	}
 }
 
 func (c *Client) connect() error {
-    c.Lock()
-    defer c.Unlock()
+	c.Lock()
+	defer c.Unlock()
 
-    dial := websocket.Dialer{TLSClientConfig: &tls.Config{
-        InsecureSkipVerify: c.client.InsecureSkipVerify,
-    }}
+	dial := websocket.Dialer{TLSClientConfig: &tls.Config{
+		InsecureSkipVerify: c.client.InsecureSkipVerify,
+	}}
 
-    u := url.URL{Scheme: "wss", Host: c.client.Host, Path: c.client.WebsocketPath}
+	u := url.URL{Scheme: "wss", Host: c.client.Host, Path: c.client.WebsocketPath}
 
-    hashBytes, err := bcrypt.GenerateFromPassword([]byte(c.client.Auth.Password), bcrypt.DefaultCost)
-    if err != nil {
-        return fmt.Errorf("failed to hash password: %w", err)
-    }
+	hashBytes, err := bcrypt.GenerateFromPassword([]byte(c.client.Auth.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return fmt.Errorf("failed to hash password: %w", err)
+	}
 
-    creds := c.client.Auth.User + ":" + util.BytesToString(hashBytes)
-    token := base64.StdEncoding.EncodeToString(util.StringToBytes(creds))
+	creds := c.client.Auth.User + ":" + util.BytesToString(hashBytes)
+	token := base64.StdEncoding.EncodeToString(util.StringToBytes(creds))
 
-    reqHeader := make(http.Header)
-    reqHeader.Add("ID", c.ID)
-    reqHeader.Add("Authorization", token)
+	reqHeader := make(http.Header)
+	reqHeader.Add("ID", c.ID)
+	reqHeader.Add("Authorization", token)
 
-    c.conn, _, err = dial.Dial(u.String(), reqHeader)
-    if err != nil {
-        return fmt.Errorf("failed to dial(%s): %w", u.String(), err)
-    }
+	c.conn, _, err = dial.Dial(u.String(), reqHeader)
+	if err != nil {
+		return fmt.Errorf("failed to dial(%s): %w", u.String(), err)
+	}
 
-    log.Infof("Succeed connecting to %s, ID: %s.", u.String(), c.ID)
+	log.Infof("Succeed connecting to %s, ID: %s.", u.String(), c.ID)
 
-    // handshake with midgard server
-    err = c.conn.WriteMessage(websocket.BinaryMessage, (&util.WebsocketMessage{
-        Action: util.ActionHandshakeRegister,
-        UserID: c.ID,
-        Data:   nil,
-    }).Encode())
+	// handshake with server
+	c.conn.SetWriteDeadline(time.Now().Add(10 * time.Second))
+	err = c.conn.WriteMessage(websocket.BinaryMessage, (&util.WebsocketMessage{
+		Action: util.ActionHandshakeRegister,
+		UserID: c.ID,
+		Data:   nil,
+	}).Encode())
 
-    if err != nil {
-        return fmt.Errorf("failed to send handshake message: %w", err)
-    }
+	if err != nil {
+		return fmt.Errorf("failed to send handshake message: %w", err)
+	}
 
-    _, msg, err := c.conn.ReadMessage()
-    if err != nil {
-        return fmt.Errorf("failed to read message for handshake: %w", err)
-    }
+	c.conn.SetReadDeadline(time.Now().Add(10 * time.Second))
+	_, msg, err := c.conn.ReadMessage()
+	if err != nil {
+		return fmt.Errorf("failed to read message for handshake: %w", err)
+	}
 
-    wsm := &util.WebsocketMessage{}
-    err = wsm.Decode(msg)
-    if err != nil {
-        return fmt.Errorf("failed to handshake with server: %w", err)
-    }
+	wsm := &util.WebsocketMessage{}
+	err = wsm.Decode(msg)
+	if err != nil {
+		return fmt.Errorf("failed to handshake with server: %w", err)
+	}
 
-    switch wsm.Action {
-    case util.ActionHandshakeReady:
-        if wsm.UserID != c.ID {
-            c.ID = wsm.UserID
-            log.Infoln("conflict hostname, updated daemon id: ", c.ID)
-        }
-    default:
-        // close the connection if handshake is not ready
-        c.conn.Close()
-        return fmt.Errorf("failed to handshake with server: %w", err)
-    }
+	switch wsm.Action {
+	case util.ActionHandshakeReady:
+		if wsm.UserID != c.ID {
+			c.ID = wsm.UserID
+			log.Infoln("conflict hostname, updated daemon id: ", c.ID)
+		}
+	default:
+		// close the connection if handshake is not ready
+		c.conn.Close()
+		return fmt.Errorf("failed to handshake with server: %w", err)
+	}
 
-    return nil
+	return nil
 }
 
-// wsReconnect tries to reconnect to the midgard server and returns
+// wsReconnect tries to reconnect to the server and returns
 // until it connects to the server.
 func (c *Client) reconnect(ctx context.Context) {
-    tk := time.NewTicker(10 * time.Second)
-    defer tk.Stop()
-    for {
-        select {
-        case <-ctx.Done():
-            return
-        case <-tk.C:
-            err := c.connect()
-            if err == nil {
-                log.Infoln("connected to server succeed.")
-                return
-            }
-            log.Errorf("%v\n", err)
-            log.Infoln("retry in 10 seconds..")
-        }
-    }
+	tk := time.NewTicker(10 * time.Second)
+	defer tk.Stop()
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case <-tk.C:
+			err := c.connect()
+			if err == nil {
+				log.Infoln("connected to server succeed.")
+				return
+			}
+			log.Errorf("%v\n", err)
+			log.Infoln("retry in 10 seconds..")
+		}
+	}
 }
 
-func (c *Client) handleIO(ctx context.Context) {
-    if c.conn == nil {
-        c.reconnect(ctx)
-    }
+func (c *Client) handleIO(ctx context.Context, clipData <-chan []byte) {
+	if c.conn == nil {
+		c.reconnect(ctx)
+	}
 
-    log.Infoln("Client id:", c.ID)
-    go c.readFromServer(ctx)
-    c.writeToServer(ctx)
-    c.close()
+	log.Infoln("Client id:", c.ID)
+	go c.watchClipboard(ctx, clipData)
+	go c.readFromServer(ctx)
+	go c.writeToServer(ctx)
+	//c.close()
 }
 
 func (c *Client) readFromServer(ctx context.Context) {
-    for {
-        select {
-        case <-ctx.Done():
-            return
-        default:
-            _, msg, err := c.conn.ReadMessage()
-            if err != nil {
-                log.Errorf("Failed to read message from server: %v", err)
+	for {
+		select {
+		case <-ctx.Done():
+			log.Infoln("Exit read routine.")
+			return
+		default:
+			_, msg, err := c.conn.ReadMessage()
+			if err != nil {
+				log.Errorf("Failed to read message from server: %v", err)
 
-                c.Lock()
-                c.conn = nil
-                c.Unlock()
+				c.Lock()
+				c.conn = nil
+				c.Unlock()
 
-                // block until connection is ready again
-                c.reconnect(ctx)
-                continue
-            }
+				// block until connection is ready again
+				c.reconnect(ctx)
+				continue
+			}
 
-            wsm := &util.WebsocketMessage{}
-            err = wsm.Decode(msg)
-            if err != nil {
-                log.Printf("failed to read message: %v", err)
-                continue
-            }
+			wsm := &util.WebsocketMessage{}
+			err = wsm.Decode(msg)
+			if err != nil {
+				log.Printf("failed to read message: %v", err)
+				continue
+			}
 
-            // duplicate messages to all readers, readers should not edit the message
-            c.readChs.Range(func(k, v interface{}) bool {
-                readerCh := v.(chan *util.WebsocketMessage)
-                readerCh <- wsm
-                return true
-            })
+			// duplicate messages to all readers, readers should not edit the message
+			c.readChs.Range(func(k, v interface{}) bool {
+				readerCh := v.(chan *util.WebsocketMessage)
+				readerCh <- wsm
+				return true
+			})
 
-            switch wsm.Action {
-            case util.ActionClipboardChanged:
-                log.Infof("Clipboard data has changed from %s, sync with local...", wsm.UserID)
-                clipboard.Write(wsm.Data)
-                log.Infof("Clipboard data has changed from %s, sync succeed.", wsm.UserID)
-            }
-        }
-    }
+			switch wsm.Action {
+			case util.ActionClipboardChanged:
+				log.Infof("Clipboard data has changed from %s, sync with local...", wsm.UserID)
+				clipboard.Write(wsm.Data)
+				log.Infof("Clipboard data has changed from %s, sync succeed.", wsm.UserID)
+			}
+		}
+	}
 }
 
 func (c *Client) writeToServer(ctx context.Context) {
-    for {
-        select {
-        case <-ctx.Done():
-            if c.conn == nil {
-                log.Errorln("Connection was not ready for user:", c.ID)
-            }
-            return
-        case msg := <-c.writeCh:
-            if c.conn == nil {
-                log.Errorln("connection is not ready yet for user:", c.ID)
-                continue
-            }
+	for {
+		select {
+		case <-ctx.Done():
+			log.Infoln("Exit write routine.")
+			if c.conn == nil {
+				log.Errorln("Connection was not ready for user:", c.ID)
+			}
+			return
+		case msg := <-c.writeCh:
+			if c.conn == nil {
+				log.Errorln("connection is not ready yet for user:", c.ID)
+				continue
+			}
 
-            err := c.conn.WriteMessage(websocket.BinaryMessage, msg.Encode())
-            if err != nil {
-                log.Errorf("failed to write message to server: %v", err)
-                return
-            }
-        }
-    }
+			err := c.conn.WriteMessage(websocket.BinaryMessage, msg.Encode())
+			if err != nil {
+				log.Errorf("failed to write message to server: %v", err)
+				return
+			}
+		}
+	}
+}
+
+func (c *Client) watchClipboard(ctx context.Context, clipData <-chan []byte) {
+	for {
+		select {
+		case <-ctx.Done():
+			log.Infoln("Exit watch routine.")
+			if c.conn == nil {
+				log.Errorln("Connection was not ready for user:", c.ID)
+			}
+			return
+		case data, ok := <-clipData:
+			if c.conn == nil || !ok {
+				log.Errorln("connection is not ready yet for user:", c.ID)
+				continue
+			}
+
+			if !ok {
+				log.Errorln("Clipboard data channel has been closed.")
+				continue
+			}
+
+			c.writeCh <- &util.WebsocketMessage{
+				Action: util.ActionClipboardChanged,
+				UserID: c.ID,
+				Data:   data,
+			}
+		}
+	}
 }
 
 func (c *Client) close() {
-    _ = c.conn.WriteMessage(websocket.BinaryMessage, (&util.WebsocketMessage{
-        Action: util.ActionTerminate,
-        UserID: c.ID,
-    }).Encode())
+	_ = c.conn.WriteMessage(websocket.BinaryMessage, (&util.WebsocketMessage{
+		Action: util.ActionTerminate,
+		UserID: c.ID,
+	}).Encode())
 
-    h := c.conn.CloseHandler()
-    h(websocket.CloseNormalClosure, "")
+	h := c.conn.CloseHandler()
+	h(websocket.CloseNormalClosure, "")
 
-    c.conn.Close()
+	c.conn.Close()
 }
